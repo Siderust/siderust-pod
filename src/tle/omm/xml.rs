811a4e02cad7f1b45b2a8632a9f980140baac2dd
Vallés Puig, Ramon <74069811+VPRamon@users.noreply.github.com>
2026-09-21 18:48:57 +0200

    style: apply rustfmt to src/tle/omm/xml.rs

--- src/tle/omm/xml.rs
+++ src/tle/omm/xml.rs
@@ -12,8 +12,8 @@ use qtty::angular::Degrees;
 use qtty_core::units::{angular::Turn, angular_rate::AngularRate, time::Day};
 
 use super::{format_epoch, parse_epoch, Omm};
-use crate::tle::{Classification, SatelliteNumber};
 use crate::tle::TleError;
+use crate::tle::{Classification, SatelliteNumber};
 
 /// Render an [`Omm`] to its XML form (`<omm>...</omm>`).
 ///
